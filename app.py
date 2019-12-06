50dd49b40540423fa05593d8643eca23f766d86a
Rhy <yhr@xs4all.nl>
2019-12-06 11:59:06 +0000

    trying to make mongodb work with python files

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
 from flask import Flask, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+MONGO_URI = "mongodb+srv://root:1ZmNgZ@myfirstcluster-eiytd.mongodb.net/myTestDB?retryWrites=true&w=majority"
+client = MongoClient(MONGO_URI)
+db = client.myTestDB
+collection = db.Recepten
 
 @app.route('/')
 def home_page():
@@ -19,7 +25,11 @@ def northamerica():
 def asia():
     return "This is the page for Asia"
 
+@app.route('/dbplayground')
+def playground():
+    return "This is the page where we play with databases"
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
-            port=int(os.environ.get('PORT', '5000')),
-            debug=True)
+        port=int(os.environ.get('PORT', '5000')),
+        debug=True)
